d7c0c742f857cee50e5ef66c0334f9a4ca23726e
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2025-05-21 16:13:40 +0530

    add firebaseConfig

---
+++ frontend/uncle-nomad-frontend/src/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcnEPDVy1u_hD-zI-DzXX7xtcA3f232bE",
+  authDomain: "uncle-nomad-c5f9a.firebaseapp.com",
+  projectId: "uncle-nomad-c5f9a",
+  storageBucket: "uncle-nomad-c5f9a.firebasestorage.app",
+  messagingSenderId: "189010268588",
+  appId: "1:189010268588:web:c850b687fd729d6ec1aa09",
+  measurementId: "G-VB6029FQXS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+export { auth };  // ✅ Export auth for authentication
